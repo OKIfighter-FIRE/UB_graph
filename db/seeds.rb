dfb2802fcf4c0dbd4b1f6868d2ea7baae2ad0199
kinjo_s <hahaha.kinjo0330@gmail.com>
2022-06-05 10:21:02 +0900

    changed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 list = [
-  { date: Date.new(2020, 1, 1), weight: 60.3 },
+  { date: Date.new(2020, 1, 1), weight: 60.6 },
   { date: Date.new(2020, 1, 2), weight: 61.1 },
   { date: Date.new(2020, 1, 3), weight: 60.8 },
-  { date: Date.new(2020, 1, 4), weight: nil },
+  { date: Date.new(2020, 1, 4), weight: 60.6 },
   { date: Date.new(2020, 1, 5), weight: 60.5 },
-  { date: Date.new(2020, 1, 6), weight: 61.4 },
+  { date: Date.new(2020, 1, 6), weight: 61.0 },
 ]
 
 Graph.destroy_all
@@ -22,9 +22,9 @@ list = [
   { date: Date.new(2020, 1, 1), num: 60.3 },
   { date: Date.new(2020, 1, 2), num: 61.1 },
   { date: Date.new(2020, 1, 3), num: 60.8 },
-  { date: Date.new(2020, 1, 4), num: nil },
+  { date: Date.new(2020, 1, 4), num: 59.8 },
   { date: Date.new(2020, 1, 5), num: 60.5 },
-  { date: Date.new(2020, 1, 6), num: 61.4 },
+  { date: Date.new(2020, 1, 6), num: 60.1 },
 ]
 
 Profit.destroy_all
